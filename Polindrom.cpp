dbb09d380b5f661ea97165ebbb63db5bd4a3c5db
Polzovat123 <42420479+Polzovat123@users.noreply.github.com>
2020-12-12 21:18:08 +0500

    Create Polindrom.cpp

---
+++ Polindrom.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int word[1005][12];
+    //1==a
+    //2==b
+    //3==c
+    //4==_
+    word[0][1] = 1;
+    int len_element=1;
+    for(int i=2; i<=10; i++){
+      for(int j=0;j<1;j++){
+	swithc(word[j][i-1]){
+	  case 1:{
+	    break;
+	  }
